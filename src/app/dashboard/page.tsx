931efbcd8df096c5a9ed7c8b281dafdc54b02780
guusfelix2015 <gusfelix2015@gmail.com>
2025-05-27 22:22:57 -0300

    feat: add clinic creation form and dialog component

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,23 +1,36 @@
+import { eq } from "drizzle-orm";
 import { headers } from "next/headers";
 import { redirect } from "next/navigation";
 
+import { db } from "@/db";
+import { usersToClinicsTable } from "@/db/schema";
 import { auth } from "@/lib/auth";
 
 import SignOutButton from "./components/sign-out-button";
 
-export default async function DashboardPage() {
-
+const DashboardPage = async () => {
   const session = await auth.api.getSession({
     headers: await headers(),
   });
-
-  if (!session) {
+  if (!session?.user) {
     redirect("/authentication");
   }
 
-  return <div>
-    <h1>Dashboard</h1>
-    <p>{session?.user?.email}</p>
-    <SignOutButton />
-  </div>;
-}
+  const clinics = await db.query.usersToClinicsTable.findMany({
+    where: eq(usersToClinicsTable.userId, session.user.id),
+  });
+
+  if (clinics.length === 0) {
+    redirect("/clinic-form");
+  }
+  return (
+    <div>
+      <h1>Dashboard</h1>
+      <h1>{session?.user?.name}</h1>
+      <h1>{session?.user?.email}</h1>
+      <SignOutButton />
+    </div>
+  );
+};
+
+export default DashboardPage;
