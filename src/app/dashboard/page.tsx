44edc15fffb709bfcb9339c292a6e273faaa6e63
guusfelix2015 <gusfelix2015@gmail.com>
2025-05-27 21:12:24 -0300

    feat: implement authentication flow with session management and sign-out functionality

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,3 +1,23 @@
-export default function DashboardPage() {
-  return <div>Dashboard</div>;
+import { headers } from "next/headers";
+import { redirect } from "next/navigation";
+
+import { auth } from "@/lib/auth";
+
+import SignOutButton from "./components/sign-out-button";
+
+export default async function DashboardPage() {
+
+  const session = await auth.api.getSession({
+    headers: await headers(),
+  });
+
+  if (!session) {
+    redirect("/authentication");
+  }
+
+  return <div>
+    <h1>Dashboard</h1>
+    <p>{session?.user?.email}</p>
+    <SignOutButton />
+  </div>;
 }
